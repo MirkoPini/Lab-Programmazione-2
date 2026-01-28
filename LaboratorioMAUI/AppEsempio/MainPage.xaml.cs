8ff680ab07ad3515bd95e692d21ca54d155ad22e
Mirko Pini <hic866@student.edu.ti.ch>
2026-01-28 13:52:50 +0100

    Pini

---
+++ LaboratorioMAUI/AppEsempio/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace AppEsempio
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int count10 = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnCounterClickedX10(object sender, EventArgs e)
+        {
+            count10 += 10;
+            if (count10 == 1)
+                CounterBtnx10.Text = $"Clicked {count10} time";
+            else
+                CounterBtnx10.Text = $"Clicked {count10} times";
+
+            SemanticScreenReader.Announce(CounterBtnx10.Text);
+
+        }
+    }
+
+}
