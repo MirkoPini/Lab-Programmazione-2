d66a4a4b08832022c1be1ca0cf84f678607fa846
Mirko Pini <hic866@student.edu.ti.ch>
2026-02-11 13:01:21 +0100

    Pini

---
+++ LaboratorioMAUI/AppQuiz/Models/QuestionBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppQuiz.Models
+{
+    internal abstract class QuestionBase
+    {
+		private int _point;
+
+		public int Point
+		{
+			get { return _point; }
+			set 
+			{ 
+				if(value < 0)
+				{
+					_point = 0;
+				}
+				else
+				{
+                    _point = value;
+                }
+			}
+		}
+
+		private string _text;
+
+		public string Text
+		{
+			get { return _text; }
+			set { _text = value; }
+		}
+	}
+}
