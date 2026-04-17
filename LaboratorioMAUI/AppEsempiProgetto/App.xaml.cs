969ff6d1d340e996e292aa2f5a5d0a94ad1dac2d
Mirko Pini <hic866@student.edu.ti.ch>
2026-04-17 15:41:43 +0200

    Pini

---
+++ LaboratorioMAUI/AppEsempiProgetto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppEsempiProgetto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LaboratorioMAUI/AppEsempiProgetto/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace AppEsempiProgetto
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
