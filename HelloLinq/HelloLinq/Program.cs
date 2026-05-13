7d2548cf416aada90b7f8f557f04684fb0baf4a9
Mirko Pini <hic866@student.edu.ti.ch>
2026-05-13 14:39:09 +0200

    Pini

---
+++ HelloLinq/HelloLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int>() { 1, 2, 3, 4, 5, 6};
+
+            //Vogliamo estrapolare i numeri pari della lista numeri
+            //ToList() trasforma l'oggetto Ienumerable in lista
+
+            List<int> pari = numeri.Where(n => n % 2 == 0).ToList();
+
+            
+            foreach (int x in pari)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
